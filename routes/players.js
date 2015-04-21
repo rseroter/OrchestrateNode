2ffde976f6a46a445de8bba6680445319c89b958
Richard Seroter <rseroter@hotmail.com>
2015-04-21 08:52:41 -0700

    Initial commit

---
+++ routes/players.js
@@ -0,0 +1,65 @@
+﻿var express = require('express');
+var router = express.Router();
+
+//Orchestrate API token
+var token = "<key>";
+var orchestrate = require('orchestrate');
+//location reference
+orchestrate.ApiEndPoint = "<endpoint>";
+var db = orchestrate(token);
+
+/* GET all player listing. */
+router.get('/', function (req, res) {
+
+    db.list("Players")
+    .then(function (result) {
+        playerlist = result.body.results;
+        //console.log(playerlist);
+
+        res.render('players', { title: 'Players List', plist: playerlist });
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+});
+
+/* GET one player profile. */
+router.get('/:playerid', function (req, res) {
+    
+    console.log(req.params.playerid);
+    
+    //get player object
+    db.get("Players", req.params.playerid)
+    .then(function (result) {
+        player = result.body;
+        //console.log(player);
+        
+        //get graph of relationships
+        db.newGraphReader()
+        .get()
+        .from('Players', req.params.playerid)
+        .related('currentteammate')
+        .then(function (relres) {
+            playerlist = relres.body;
+            //console.log(playerlist);
+            
+            //get time-series events
+            db.newEventReader()
+            .from('Players', req.params.playerid)
+            .type('gamelog')
+            .list()
+            .then(function (evtres) {
+                gamelog = evtres.body;
+                //console.log(gamelog);
+                
+                res.render('player', { title: 'Player Profile', profile: player, plist: playerlist, elist: gamelog });
+            });   
+        });
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+});
+
+
+module.exports = router;
---
+++ routes/warmup.js
@@ -0,0 +1,106 @@
+﻿var express = require('express');
+var router = express.Router();
+
+//Orchestrate API token
+var token = "<key>";
+var orchestrate = require('orchestrate');
+//location reference
+orchestrate.ApiEndPoint = "<endpoint>";
+var db = orchestrate(token);
+
+/* Warmup the collection. */
+router.get('/', function (req, res) {
+    
+    //create key/value records
+    db.put('Players', "10001", {
+        "name": "Blake Griffin",
+        "team": "Los Angeles Clippers",
+        "position": "power forward",
+        "birthdate": "03/16/89",
+        "college": "Oklahoma",
+        "careerppg": "21.5",
+        "careerrpg": "9.7"
+    })
+    .then(function (result1) {
+        console.log("record added");
+    });
+    
+    //create key/value records
+    db.put('Players', "10002", {
+        "name": "DeAndre Jordan",
+        "team": "Los Angeles Clippers",
+        "position": "center",
+        "birthdate": "07/21/88",
+        "college": "Texas A&M",
+        "careerppg": "8.0",
+        "careerrpg": "9.0"
+    })
+    .then(function (result2) {
+        console.log("record added");
+    });
+    
+    //create key/value records
+    db.put('Players', "10003", {
+        "name": "Matt Barnes",
+        "team": "Los Angeles Clippers",
+        "position": "strong forward",
+        "birthdate": "03/09/80",
+        "college": "UCLA",
+        "careerppg": "8.1",
+        "careerrpg": "4.5",
+        "teams": [
+            "Los Angeles Clippers",
+            "Sacramento Kings",
+            "Golden State Warriors"
+        ]
+    })
+    .then(function (result3) {
+        console.log("record added");
+    });
+    
+    //create event
+    db.newEventBuilder()
+    .from('Players', '10001')
+    .type('gamelog')
+    .time(1429531200)
+    .data({ "opponent": "San Antonio Spurs", "minutes": "43", "points": "26", "rebounds": "12" })
+    .create()
+    .then(function (result4) {
+        console.log("event added"); 
+    });
+    
+    //create event
+    db.newEventBuilder()
+    .from('Players', '10001')
+    .type('gamelog')
+    .time(1429012800)
+    .data({ "opponent": "Phoenix Suns", "minutes": "29", "points": "20", "rebounds": "8" })
+    .create()
+    .then(function (result5) {
+        console.log("event added");
+    });
+    
+    //create graph relationship
+    db.newGraphBuilder()
+    .create()
+    .from('Players', '10001')
+    .related('currentteammate')
+    .to('Players', '10002')
+    .then(function (result6) {
+        console.log("graph item added");
+    });
+    
+    //create graph relationship
+    db.newGraphBuilder()
+    .create()
+    .from('Players', '10001')
+    .related('currentteammate')
+    .to('Players', '10003')
+    .then(function (result7) {
+        console.log("graph item added");
+    });
+    
+    res.send('warmed up');
+});
+
+module.exports = router;
